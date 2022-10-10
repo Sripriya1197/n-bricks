5d34564ecb6d1155d72715b2f658b8785d14ffb0
pranjal161 <161pranjal@gmail.com>
2022-10-10 12:20:20 +0100

    inital commit

---
+++ src/containers/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+    HOME
+    <a style={{fontSize: '90px'}} href="/signup">click</a>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/containers/Signup.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Signup() {
+  return (
+    <div>
+    SIgnup
+    </div>
+  );
+}
+
+export default Signup;
