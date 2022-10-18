2ca41b2cdda68670acd6adad12accb383555782c
pranjal161 <161pranjal@gmail.com>
2022-10-18 17:11:07 +0100

    Adding label & API integration

--- src/containers/Signup/Signup.tsx
+++ src/containers/Signup/Signup.tsx
@@ -27,7 +27,6 @@ const Signup = () => {
   const [payload, updatePayload] = useState<any>(defaultPayload);
 
   const createSignupPayload = (event: any, group?: any) => {
-
       getValidation(event);
       if (Object.hasOwn(payload, group)) {
         Object.assign(payload, { [group]: { ...payload[group], ...{ [event.target.id]: event.target.value } } });
@@ -51,7 +50,7 @@ const Signup = () => {
     const form = e.currentTarget;
     e.preventDefault();
     if (form.checkValidity()) {
-      axios.post(' http://localhost:8080/signup', payload).then((response) => {
+      axios.post(process.env.HOST_URL + '/signup', payload).then((response) => {
         if (response && response.data && response.data.hasOwnProperty('userId')) {
           alert("Sign up Successful");
         }
@@ -66,90 +65,97 @@ const Signup = () => {
         <div className="form-group text-center">
           <h2>Please Sign up</h2>
         </div>
-        <div className="form-group input-group">
-          <Text className="form-control required" id="firstName" placeholder="First Name" required={true} onChange={(value: any) => createSignupPayload(value)} />
-          <span className='required'></span>
+        <div className="form-group required">
+          <label>First Name</label>
+          <Text className="form-control" id="firstName" placeholder="First Name" required={true} onChange={(value: any) => createSignupPayload(value)} />
         </div>
-        <div className="form-group input-group">
+        <div className="form-group">
+        <label>Middle Name</label>
           <Text className="form-control" id="middleName" placeholder="Middle Name" onChange={(value: any) => createSignupPayload(value)} />
-          <span className='required'></span>
         </div>
-        <div className="form-group input-group">
-          <Text className="form-control required" id="lastName" placeholder="Last Name" required={true} onChange={(value: any) => createSignupPayload(value)} />
-          <span className='required'></span>
+        <div className="form-group required">
+        <label>Last Name</label>
+          <Text className="form-control" id="lastName" placeholder="Last Name" required={true} onChange={(value: any) => createSignupPayload(value)} />
         </div>
-        <div className="form-group input-group">
+        <div className="form-group required">
+        <label>User ID</label>
           <Text className={userIdValidation ? "form-control is-valid" : "form-control"} id="id" placeholder="User ID" minLength={8} maxLength={20} required={true} onChange={(value: any) => createSignupPayload(value)} />
-          <span className='required'></span>
           <div style={{ width: 'inherit' }}>
             <small>Must be unique and 8-20 characters long.</small>
           </div>
         </div>
-        <div className="form-group input-group">
+        <div className="form-group required">
+        <label>Password</label>
           <Text className={pwdValidation ? "form-control is-valid" : "form-control"} id="password" type='password' placeholder="Password" minLength={8} maxLength={20} required={true} onChange={(value: any) => createSignupPayload(value)} />
-          <span className='required'></span>
           <div style={{ width: 'inherit' }}>
             <small>Must be 8-20 characters long, atleast one letter and one number.</small>
           </div>
         </div>
-        <div className="form-group input-group">
+        <div className="form-group required">
+        <label>Hint Question</label>
           <Select className="form-control" id="hintQuestion" value={payload.passwordRecovery.hintQuestion} placeholder="Hint Question" options={constant.hintQuestions} onChange={(value: any) => createSignupPayload(value, 'passwordRecovery')} />
         </div>
-        <div className="form-group input-group">
-          <Text className="form-control required" id="passwordHint" type='password' placeholder="Password Hint" required={true} onChange={(value: any) => createSignupPayload(value, 'passwordRecovery')} />
-          <span className='required'></span>
+        <div className="form-group required">
+        <label>Password Hint</label>
+          <Text className="form-control" id="passwordHint" type='password' placeholder="Password Hint" required={true} onChange={(value: any) => createSignupPayload(value, 'passwordRecovery')} />
           <div style={{ width: 'inherit' }}>
             <small>Remember this to recover your User ID / Password.</small>
           </div>
         </div>
         <div className="form-group">
+          <label>Home Phone</label>
           <Text className="form-control" type="number" id="homePhone" placeholder="Home Phone" onChange={(value: any) => createSignupPayload(value, 'contact')} />
         </div>
-        <div className="form-group input-group">
-          <Text className="form-control required" id="mobilePhone" type="number" placeholder="Mobile Phone" required={true} onChange={(value: any) => createSignupPayload(value, 'contact')} />
-          <span className='required'></span>
+        <div className="form-group required">
+          <label>Mobile Phone</label>
+          <Text className="form-control" id="mobilePhone" type="number" placeholder="Mobile Phone" required={true} onChange={(value: any) => createSignupPayload(value, 'contact')} />
         </div>
-        <div className="form-group input-group">
-          <Text type="email" className="form-control required" id="email" placeholder="Email" required={true} onChange={(value: any) => createSignupPayload(value, 'contact')} />
-          <span className='required'></span>
+        <div className="form-group required">
+          <label>Email</label>
+          <Text type="email" className="form-control" id="email" placeholder="Email" required={true} onChange={(value: any) => createSignupPayload(value, 'contact')} />
         </div>
-        <div className="form-group input-group">
+        <div className="form-group required">
+        <label>Classification</label>
           <Select className="form-control" id="userClassification" placeholder="Classification" value={payload.userClassification} options={constant.userClassification} onChange={(value: any) => createSignupPayload(value)} />
         </div>
         <div className="form-group">
+        <label>Role</label>
           <Select className="form-control" id="role" placeholder="Role" value={payload.role} options={constant.role} onChange={(value: any) => createSignupPayload(value)} />
         </div>
         <div className="form-group">
+        <label>Name of your Business</label>
           <Text className="form-control" id="nameOfBusiness" placeholder="Name of your Business" required={true} onChange={(value: any) => createSignupPayload(value, 'businessInfo')} />
         </div>
-        <div className="form-group input-group">
-          <Text className="form-control required" id="businessStructure" placeholder="Inc, LLC, Ltd" required={true} onChange={(value: any) => createSignupPayload(value, 'businessInfo')} />
-          <span className='required'></span>
+        <div className="form-group required">
+        <label>Inc, LLC, Ltd</label>
+          <Text className="form-control" id="businessStructure" placeholder="Inc, LLC, Ltd" required={true} onChange={(value: any) => createSignupPayload(value, 'businessInfo')} />
         </div>
-        <div className="form-group input-group">
-          <Text className="form-control required" id="streetAddres" placeholder="Street address, P.O. Box, c/o" required={true} onChange={(value: any) => createSignupPayload(value, 'address')} />
-          <span className='required'></span>
+        <div className="form-group required">
+        <label>Street address, P.O. Box, c/o</label>
+          <Text className="form-control" id="streetAddres" placeholder="Street address, P.O. Box, c/o" required={true} onChange={(value: any) => createSignupPayload(value, 'address')} />
         </div>
         <div className="form-group">
+        <label>Suite, unit, building, floor etc.</label>
           <Text className="form-control" id="infraStructureType" placeholder="Suite, unit, building, floor etc." onChange={(value: any) => createSignupPayload(value, 'address')} />
         </div>
-        <div className="form-group input-group">
-          <Text className="form-control required" id="city" placeholder="City" required={true} onChange={(value: any) => createSignupPayload(value, 'address')} />
-          <span className='required'></span>
+        <div className="form-group required">
+          <label>City</label>
+          <Text className="form-control" id="city" placeholder="City" required={true} onChange={(value: any) => createSignupPayload(value, 'address')} />
         </div>
-        <div className="form-group input-group">
-          <Text className="form-control required" id="state" placeholder="State/Province/Region" required={true} onChange={(value: any) => createSignupPayload(value, 'address')} />
-          <span className='required'></span>
+        <div className="form-group required">
+          <label>State/Province/Region</label>
+          <Text className="form-control" id="state" placeholder="State/Province/Region" required={true} onChange={(value: any) => createSignupPayload(value, 'address')} />
         </div>
-        <div className="form-group input-group">
-          <Text className="form-control required" id="postalCode" placeholder="Postal Code" required={true} onChange={(value: any) => createSignupPayload(value, 'address')} />
-          <span className='required'></span>
+        <div className="form-group required">
+        <label>Postal Code</label>
+          <Text className="form-control" id="postalCode" placeholder="Postal Code" required={true} onChange={(value: any) => createSignupPayload(value, 'address')} />
         </div>
         <div className="form-group">
+        <label>Country</label>
           <Select className="form-control" id="country" placeholder="Country" options={constant.countries} value={payload.address.country} onChange={(value: any) => createSignupPayload(value, 'address')} />
         </div>
         <div className="form-group">
-          <small id="signupterms" className="text-muted">By signing up you agree to our <a href="/termsofservice">Terms of Service</a>.</small>
+          <small id="signupterms" className="text-muted">By signing up you agree to our <a href='' onClick={() => window.open('https://www.n-bricks.com/termsofservice')}>Terms of Service</a>.</small>
         </div>
         <div className="form-group">
           <div className="g-recaptcha" style={{marginLeft: '7rem'}} data-sitekey="6LeNQ4YiAAAAACr0Ds8VmpuDdrQ76oFFfYyLAAkN"></div>
